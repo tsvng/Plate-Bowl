683388fc629d3bd32fc8000e09b643685a5c3c5f
Anthony Nguyen <anthonydtnguyen@gmail.com>
2019-11-09 14:24:47 -0800

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,7 @@ module.exports = {
         hot: true
     },
     plugins: [
-        new CopyWebpackPlugin(['index.html','test.html','style.css']),
+        new CopyWebpackPlugin(['index.html','test.html','style.css','home.html']),
         new webpack.HotModuleReplacementPlugin()
     ]
 };
