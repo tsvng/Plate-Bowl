ee40f77e33e4514bf32c5ea1a936cb70ab36b9e9
Anthony Nguyen <anthonydtnguyen@gmail.com>
2019-11-09 15:41:00 -0800

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,7 @@ module.exports = {
         hot: true
     },
     plugins: [
-        new CopyWebpackPlugin(['index.html','style.css','home.html','RecommendFood.html']),
+        new CopyWebpackPlugin(['index.html','style.css','home.html','RecommendFood.html','src/assets/exampleimage.jpg']),
         new webpack.HotModuleReplacementPlugin()
     ]
 };
