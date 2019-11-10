8150238d6a57e8548342fd2bdcded7e98e401289
jrasmussenjr <rasmussenjoejr@gmail.com>
2019-11-10 02:23:45 -0800

    Added groundwork for secure login

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import API, { graphqlOperation } from '@aws-amplify/api'
 import PubSub from '@aws-amplify/pubsub';
 import { createTodo } from './graphql/mutations'
 
+import { Auth } from 'aws-amplify';
 import awsconfig from './aws-exports';
 API.configure(awsconfig);
 PubSub.configure(awsconfig);
@@ -11,12 +12,15 @@ async function createNewTodo() {
   return await API.graphql(graphqlOperation(createTodo, { input: todo }))
 }
 
-const MutationButton = document.getElementById('MutationEventButton');
-const MutationResult = document.getElementById('MutationResult');
+const LoginButton = document.getElementById('LoginButton');
+const SignupButton = document.getElementById('SignupButton');
 
-MutationButton.addEventListener('click', (evt) => {
-  MutationResult.innerHTML = `MUTATION RESULTS:`;
-  createNewTodo().then( (evt) => {
-    MutationResult.innerHTML += `<p>${evt.data.createTodo.name} - ${evt.data.createTodo.description}</p>`
+LoginButton.addEventListener('click', (event) => {
+    Auth.federatedSignIn();
+  })
+});
+
+SignupButton.addEventListener('click', (event) => {
+    Auth.federatedSignIn();
   })
 });
