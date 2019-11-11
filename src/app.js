d7d6121a7abe15db167de95c5cdd76528c3343c7
jrasmussenjr <rasmussenjoejr@gmail.com>
2019-11-11 14:22:17 -0800

    Manual Auth configuration due to lacking config.

--- src/app.js
+++ src/app.js
@@ -2,10 +2,22 @@ import API, { graphqlOperation } from '@aws-amplify/api'
 import PubSub from '@aws-amplify/pubsub';
 import { createTodo } from './graphql/mutations'
 import Auth from '@aws-amplify/auth';
+
 import awsconfig from './aws-exports';
 API.configure(awsconfig);
 PubSub.configure(awsconfig);
-Auth.configure(awsconfig);
+
+
+Auth.configure({
+    identityPoolId: process.env.AWS_IDENTITY_POOL_ID,
+    mandatorySignIn: true,
+    region: process.env.AWS_REGION,
+    userPoolId: process.env.AWS_USER_POOL_ID,
+    userPoolWebClientId: process.env.AWS_USER_POOL_CLIENT_ID,
+
+    cookieStorage: {
+      domain: process.env.AWS_COOKIE_DOMAIN
+    });
 
 const LoginButton = document.getElementById('LoginButton');
 const SignupButton = document.getElementById('SignupButton');
