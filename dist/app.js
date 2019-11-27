557fb935f522066268a3c15fd19c000c0a2a05d1
wikLiA <notknownanonymous@gmail.com>
2019-11-27 00:44:29 -0800

    local changes while accessing from laptop

--- dist/app.js
+++ dist/app.js
@@ -72,7 +72,7 @@ class App extends React.Component {
   render() {
     if (this.state.user == null) {
      return (
-      <ImageBackground source = {'../assets/background.jpg'} style = {{width: '100%', height: '100%'}}>
+      <ImageBackground source = {'./assets/background.jpg'} style = {{width: '100%', height: '100%'}}>
        <div>
           <Login/>
        </div>
