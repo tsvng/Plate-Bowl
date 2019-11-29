00b9344ddf2fe37f959cf315e4bf53c608f3edbb
wikLiA <notknownanonymous@gmail.com>
2019-11-29 15:20:37 -0800

    syntax

--- src/Leaderboard.js
+++ src/Leaderboard.js
@@ -148,7 +148,7 @@ export default class LeaderBoard extends React.Component {
       <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Bowlby+One" />
           <button id='MutationEventButton'>Change Leaderboard</button>
           <div id='MutationResult'></div>
-          <div id='QueryResult' style={"overflow-y:scroll;"}></div>
+          <div id='QueryResult' style={overflow-y: 'scroll'}></div>
         </div>
 
   }
