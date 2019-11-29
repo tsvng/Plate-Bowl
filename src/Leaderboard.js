2140c85af506d70abbad59b91b0b07edd49480a6
wikLiA <notknownanonymous@gmail.com>
2019-11-28 19:47:19 -0800

    Update Leaderboard.js

--- src/Leaderboard.js
+++ src/Leaderboard.js
@@ -83,7 +83,7 @@ export default class LeaderBoard extends React.Component {
     	}
 	render() {
 		return <div id='main' className = "leaderboard">
-					<h1>Leaderboard</h1>
+					<h1>${currentUser}'s Leaderboard</h1>
 					<div id='QueryResult'></div>
 				</div>
 
