3d618674c195c321900c99d96b61752677ba8354
Eileen Lu <Eileen@Eileens-MacBook-Air.local>
2019-11-29 17:25:22 -0800

    Added home page

--- src/Home.js
+++ src/Home.js
@@ -1,29 +1,37 @@
-import React from 'react';
+import React, { useEffect } from 'react';import NavBar from './NavBar.js';
+import LeaderBoard from './Leaderboard.js';
+import BucketList from './BucketList.js';
+import FoodHistory from './FoodHistory.js';
+import RecommendFood from './RecommendFood.js';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 
 export default class Home extends React.Component{
-	render(){
-		return (
-			<div>
-				<div id = "main">	
-				    <div className="app">
-				        <div className="app-header">
+
+  render() { return <div id = "home" className = "app">	
+        <div class = "nav">
+      <div class = "nav-right">
+        <a href="#home">Home</a>
+        <a href="#leaderboard">Leaderboard</a>
+        <a href="#recommendFood">Recommendations</a>
+        <a href="#foodHistory">History</a>
+        <a href="#bucketList">Bucket List</a>
+      </div>
+      </div>
+        <div className = "container2">
+				        <div id = "login-page" className="app-header">
 				            <div className="app-logo">
-				                <img src="https://amplify-platenbowl-test-154226-deployment.s3.amazonaws.com/assets/PBLogo.png" style={{width:'200px'}} />
+				                <img src="https://amplify-platenbowl-test-154226-deployment.s3.amazonaws.com/assets/PBLogo.png" style={{width:'400px'}} />
 				            </div>
-				            <h1>Home Page</h1>
+				            <h3>Welcome Back!</h3>
 				        </div>
 				    </div>
-				    <div id="bucketPreview" className="preview">
-				    	<h1>Your Bucket List</h1>
-				    </div>
-				    <div id="foodHistPreview" className="preview">
-				    	<h1>Your Food History</h1>
-				    </div>
-				    <div id="leaderBoard" className= "preview">
-				    	<h1>Leaderboard</h1>
-				    </div>
 			    </div>
-			</div>
-		);
 	}
 }
+
