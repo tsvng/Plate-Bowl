2de7f40e205788d6263779bbe23dc70a474298e9
anthonydtnguyen <37089424+anthonydtnguyen@users.noreply.github.com>
2019-11-30 15:37:45 -0800

    Update Leaderboard.js
    testing sorting function

--- src/Leaderboard.js
+++ src/Leaderboard.js
@@ -56,13 +56,16 @@ export default class LeaderBoard extends React.Component {
         // Start by saying there should be no switching:
         for (i = 1; i < (rows.length - 1); i++) {
           shouldSwitch = false;
+          console.log(rows.lenght);
           /* Get the two elements you want to compare,
           one from current row and one from the next: */
           x = rows[i].getElementsByTagName("TD")[1];
           y = rows[i + 1].getElementsByTagName("TD")[1];
           /* Check if the two rows should switch place,
           based on the direction, asc or desc: */
-
+          console.log(x.innerHTML);
+          console.log(x);
+          console.log(y.innerHTML)
           if (x.innerHTML < y.innerHTML) {
             // If so, mark as a switch and break the loop:
             shouldSwitch = true;
@@ -70,6 +73,7 @@ export default class LeaderBoard extends React.Component {
           }
         }         
         if (shouldSwitch) {
+          console.log("shouldSwitchHERE HI");
           /* If a switch has been marked, make the switch
           and mark that a switch has been done: */
           rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
